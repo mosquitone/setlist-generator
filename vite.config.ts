fd1c735c4fa8d0f46b688417f04f51fbf8d1692e
yyamashita <yyamashita@mosquit.one>
2024-03-12 10:12:15 +0900

    support vercel cli dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import browserslistToEsbuild from 'browserslist-to-esbuild'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.PORT) || undefined,
+  },
   build: { target: browserslistToEsbuild() , outDir: "build"} ,
   plugins: [react()],
 })
